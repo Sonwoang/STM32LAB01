7b29035a978cb28f49148af83eaa42c1d43383c4
sonnguyenhoang9452 <son.nguyenhoang9452@hcmut.edu.vn>
2024-12-17 04:43:11 +0700

    EX6

---
+++ Sourcecode/Core/Inc/EX6.h
@@ -0,0 +1,14 @@
+/*
+ * EX6.h
+ *
+ *  Created on: Dec 17, 2024
+ *      Author: LENOVO
+ */
+
+#ifndef INC_EX6_H_
+#define INC_EX6_H_
+
+#include "stm32f1xx_hal.h"
+void Ex6(void);
+
+#endif /* INC_EX6_H_ */
---
+++ Sourcecode/Core/Src/EX6.c
@@ -0,0 +1,26 @@
+/*
+ * EX6.c
+ *
+ *  Created on: Dec 17, 2024
+ *      Author: LENOVO
+ */
+#include "Ex6.h"
+
+void Ex6(void){
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, RESET);
+
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, RESET);
+
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, RESET);
+
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, RESET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, RESET);
+}
+
